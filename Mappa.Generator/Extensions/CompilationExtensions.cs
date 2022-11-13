6df0af1a30f09b7529a0fce44748f20ac2f7f54e
Stefano Anelli <stefano.anelli@gmail.com>
2022-11-13 23:25:45 +0000

    Added more extension methods

--- Mappa.Generator/Extensions/CompilationExtensions.cs
+++ Mappa.Generator/Extensions/CompilationExtensions.cs
@@ -32,7 +32,41 @@ internal static class CompilationExtensions
     /// <typeparam name="TType">The type to be obtained.</typeparam>
     /// <param name="compilation">The current compilation.</param>
     /// <returns>The <see cref="INamedTypeSymbol"/> for type <typeparamref name="TType"/>.</returns>
-    /// <exception cref="MappaGeneratorException">When <paramref name="type"/> cannot be loaded.</exception>
+    /// <exception cref="MappaGeneratorException">When <typeparamref name="TType"/> cannot be loaded.</exception>
     internal static INamedTypeSymbol GetTypeSymbol<TType>(this Compilation compilation)
         => compilation.GetTypeSymbol(typeof(TType));
+
+    /// <summary>
+    /// Check if an implic conversion from <paramref name="sourceType"/> to
+    /// <paramref name="targetType"/> exists.
+    /// </summary>
+    /// <param name="compilation">The compilation.</param>
+    /// <param name="targetType">The target type.</param>
+    /// <param name="sourceType">The source type.</param>
+    /// <returns>
+    /// <c>true</c> if an implic conversion from <paramref name="sourceType"/> to
+    /// <paramref name="targetType"/> exists, <c>false</c> otherwise.
+    /// </returns>
+    internal static bool HasImplicitConversion(this Compilation compilation, ITypeSymbol targetType, ITypeSymbol sourceType)
+    {
+        var conversion = compilation.ClassifyCommonConversion(sourceType, targetType);
+        return conversion.Exists && conversion.IsImplicit;
+    }
+
+    /// <summary>
+    /// Check if an explicit non-numeric conversion from <paramref name="sourceType"/> to
+    /// <paramref name="targetType"/> exists.
+    /// </summary>
+    /// <param name="compilation">The compilation.</param>
+    /// <param name="targetType">The target type.</param>
+    /// <param name="sourceType">The source type.</param>
+    /// <returns>
+    /// <c>true</c> if an explicit non-numeric conversion from <paramref name="sourceType"/> to
+    /// <paramref name="targetType"/> exists, <c>false</c> otherwise.
+    /// </returns>
+    internal static bool HasExplicitNonNumericConversion(this Compilation compilation, ITypeSymbol targetType, ITypeSymbol sourceType)
+    {
+        var conversion = compilation.ClassifyCommonConversion(sourceType, targetType);
+        return conversion.Exists && !conversion.IsImplicit && !conversion.IsNumeric;
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Mappa
 Mapper using source generators
+
+## Notes
+In order to be able to compile you need to setup a `nuget.config` like the following:
+```XML
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+    <packageSources>
+        <add key="NuGet.org" value="https://api.nuget.org/v3/index.json" />
+        <add key="Local .packages" value="file://{PATH-TO-YOUR-REPO}/.packages" />
+    </packageSources>
+</configuration>
+```
+
+## References
+- [Roslyn API FAQ](https://github.com/dotnet/roslyn-sdk/blob/main/samples/CSharp/APISamples/FAQ.cs)
+- [Source generator Cookbook](https://github.com/dotnet/roslyn/blob/main/docs/features/source-generators.cookbook.md)
