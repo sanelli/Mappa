237c8367c1ab21632e35bd03d1ecf17e0af5be5c
Stefano Anelli <stefano.anelli@gmail.com>
2024-03-09 21:30:59 +0000

    #42 Added more tests to cover tuple mappings

--- Mappa.Generator.Tests/TupleToTupleMapStrategyIntegrationTests.cs
+++ Mappa.Generator.Tests/TupleToTupleMapStrategyIntegrationTests.cs
@@ -12,10 +12,6 @@ namespace Mappa.Generator.Tests;
 /// <summary>
 /// Integration tests for <see cref="TupleToTupleMapStrategy"/> strategy.
 /// </summary>
-// TODO [#42] (...) -> Test Tuple<...>.
-// TODO [#42] (...) -> ( named ).
-// TODO [#42] ( named ) -> Test Tuple<...>.
-// TODO [#42] ( named ) -> (...).
 public sealed class TupleToTupleMapStrategyIntegrationTests
     : MappaGeneratorAbstractUnitTests
 {
@@ -230,6 +226,148 @@ public sealed class TupleToTupleMapStrategyIntegrationTests
                 });
     }
 
+     /// <summary>
+    /// Test a mapping can be created from a tuple with anonymous elements
+    /// to <see cref="System.Tuple{T1,T2}"/>.
+    /// </summary>
+    /// <returns>The async task.</returns>
+    [Fact]
+    [IntegrationTest]
+    public async Task CanMapTupleWithAnonymousElementsToSystemTuple()
+    {
+        // Arrange
+        const string sourceCode = """
+                                  using Mappa.Attributes;
+                                  using System;
+
+                                  namespace Mappa.Generator.Tests.UnitTests.SourceCode;
+
+                                  [Mappa]
+                                  public sealed partial class Mapper
+                                  {
+                                      public partial Tuple<string, string> Map((int, string) input);
+                                  }
+                                  """;
+
+        // Act
+        var generatedResults = await RunMappaGeneratorAsync(sourceCode, CancellationToken.None).ConfigureAwait(true);
+
+        // Assert
+        generatedResults.Should()
+            .NotHaveDiagnostics()
+            .HaveGeneratedSourceCode()
+            .WithCompilationUnit()
+            .NotBeNull().And
+            .HaveDefaultMapMethod(
+                typeof(Tuple<string, string>).ToString(),
+                NullableAnnotation.None,
+                typeof((int, string)).ToString(),
+                NullableAnnotation.NotAnnotated,
+                blockSyntaxAssertions =>
+                {
+                    blockSyntaxAssertions
+                        .HasSyntaxNodesCount(5)
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("int", "__mappa_tmp_1", initializationAssertions => initializationAssertions.BeMemberAccessExpressionSyntax("input.Item1"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("string", "__mappa_tmp_2", initializationAssertions => initializationAssertions.BeInvocationExpressionSyntax("__mappa_tmp_1.ToString"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("string", "__mappa_tmp_3", initializationAssertions => initializationAssertions.BeMemberAccessExpressionSyntax("input.Item2"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax(
+                                typeof(Tuple<string, string>).ToString(),
+                                "__mappa_tmp_4",
+                                initializationAssertions => initializationAssertions.BeObjectCreationExpressionSyntax(
+                                    typeof(Tuple<string, string>).ToString(),
+                                    [
+                                        parametersAssertions => parametersAssertions.BeIdentifierNameSyntax("__mappa_tmp_2"),
+                                        parametersAssertions => parametersAssertions.BeIdentifierNameSyntax("__mappa_tmp_3"),
+                                    ],
+                                    []));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeReturnStatement(assertion => assertion.BeIdentifierNameSyntax("__mappa_tmp_4"));
+                        });
+                });
+    }
+
+    /// <summary>
+    /// Test a mapping can be created from a tuple with anonymous elements
+    /// to a tuple with named elements.
+    /// </summary>
+    /// <returns>The async task.</returns>
+    [Fact]
+    [IntegrationTest]
+    public async Task CanMapTupleWithAnonymousElementsToTupleWithNamedElements()
+    {
+        // Arrange
+        const string sourceCode = """
+                                  using Mappa.Attributes;
+                                  using System;
+
+                                  namespace Mappa.Generator.Tests.UnitTests.SourceCode;
+
+                                  [Mappa]
+                                  public sealed partial class Mapper
+                                  {
+                                      public partial (string First, string Second) Map((int, string) input);
+                                  }
+                                  """;
+
+        // Act
+        var generatedResults = await RunMappaGeneratorAsync(sourceCode, CancellationToken.None).ConfigureAwait(true);
+
+        // Assert
+        generatedResults.Should()
+            .NotHaveDiagnostics()
+            .HaveGeneratedSourceCode()
+            .WithCompilationUnit()
+            .NotBeNull().And
+            .HaveDefaultMapMethod(
+                "(string First, string Second)",
+                NullableAnnotation.NotAnnotated,
+                typeof((int, string)).ToString(),
+                NullableAnnotation.NotAnnotated,
+                blockSyntaxAssertions =>
+                {
+                    blockSyntaxAssertions
+                        .HasSyntaxNodesCount(5)
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("int", "__mappa_tmp_1", initializationAssertions => initializationAssertions.BeMemberAccessExpressionSyntax("input.Item1"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("string", "__mappa_tmp_2", initializationAssertions => initializationAssertions.BeInvocationExpressionSyntax("__mappa_tmp_1.ToString"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("string", "__mappa_tmp_3", initializationAssertions => initializationAssertions.BeMemberAccessExpressionSyntax("input.Item2"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax(
+                                "(string First, string Second)",
+                                "__mappa_tmp_4",
+                                initializationAssertions => initializationAssertions.BeTupleExpressionSyntax(
+                                        parametersAssertions => parametersAssertions.BeIdentifierNameSyntax("__mappa_tmp_2"),
+                                        parametersAssertions => parametersAssertions.BeIdentifierNameSyntax("__mappa_tmp_3")));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeReturnStatement(assertion => assertion.BeIdentifierNameSyntax("__mappa_tmp_4"));
+                        });
+                });
+    }
+
     /// <summary>
     /// Test a mapping can be created between two tuple
     /// with anonymous elements.
@@ -287,6 +425,148 @@ public sealed class TupleToTupleMapStrategyIntegrationTests
                         {
                             syntaxNodeAssertions.BeLocalDeclarationStatementSyntax(
                                 typeof((string, string)).ToString(),
+                                "__mappa_tmp_4",
+                                initializationAssertions => initializationAssertions.BeTupleExpressionSyntax(
+                                        parametersAssertions => parametersAssertions.BeIdentifierNameSyntax("__mappa_tmp_2"),
+                                        parametersAssertions => parametersAssertions.BeIdentifierNameSyntax("__mappa_tmp_3")));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeReturnStatement(assertion => assertion.BeIdentifierNameSyntax("__mappa_tmp_4"));
+                        });
+                });
+    }
+
+    /// <summary>
+    /// Test a mapping can be created between from tuple
+    /// with named elements to <see cref="Tuple{T1,T2}"/>.
+    /// </summary>
+    /// <returns>The async task.</returns>
+    [Fact]
+    [IntegrationTest]
+    public async Task CanMapTupleWithNamedElementsToSystemTuple()
+    {
+        // Arrange
+        const string sourceCode = """
+                                  using Mappa.Attributes;
+                                  using System;
+
+                                  namespace Mappa.Generator.Tests.UnitTests.SourceCode;
+
+                                  [Mappa]
+                                  public sealed partial class Mapper
+                                  {
+                                      public partial Tuple<string, string> Map((int Alfa, string Beta) input);
+                                  }
+                                  """;
+
+        // Act
+        var generatedResults = await RunMappaGeneratorAsync(sourceCode, CancellationToken.None).ConfigureAwait(true);
+
+       // Assert
+        generatedResults.Should()
+            .NotHaveDiagnostics()
+            .HaveGeneratedSourceCode()
+            .WithCompilationUnit()
+            .NotBeNull().And
+            .HaveDefaultMapMethod(
+                typeof(Tuple<string, string>).ToString(),
+                NullableAnnotation.None,
+                "(int Alfa, string Beta)",
+                NullableAnnotation.NotAnnotated,
+                blockSyntaxAssertions =>
+                {
+                    blockSyntaxAssertions
+                        .HasSyntaxNodesCount(5)
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("int", "__mappa_tmp_1", initializationAssertions => initializationAssertions.BeMemberAccessExpressionSyntax("input.Item1"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("string", "__mappa_tmp_2", initializationAssertions => initializationAssertions.BeInvocationExpressionSyntax("__mappa_tmp_1.ToString"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("string", "__mappa_tmp_3", initializationAssertions => initializationAssertions.BeMemberAccessExpressionSyntax("input.Item2"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax(
+                                typeof(Tuple<string, string>).ToString(),
+                                "__mappa_tmp_4",
+                                initializationAssertions => initializationAssertions.BeObjectCreationExpressionSyntax(
+                                    typeof(Tuple<string, string>).ToString(),
+                                    [
+                                        parametersAssertions => parametersAssertions.BeIdentifierNameSyntax("__mappa_tmp_2"),
+                                        parametersAssertions => parametersAssertions.BeIdentifierNameSyntax("__mappa_tmp_3"),
+                                    ],
+                                    []));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeReturnStatement(assertion => assertion.BeIdentifierNameSyntax("__mappa_tmp_4"));
+                        });
+                });
+    }
+
+     /// <summary>
+    /// Test a mapping can be created from tuple with named elements
+    /// to tuple with anonymous elements.
+    /// </summary>
+    /// <returns>The async task.</returns>
+    [Fact]
+    [IntegrationTest]
+    public async Task CanMapTupleWithNamedElementsToTupleWithAnonymousElements()
+    {
+        // Arrange
+        const string sourceCode = """
+                                  using Mappa.Attributes;
+                                  using System;
+
+                                  namespace Mappa.Generator.Tests.UnitTests.SourceCode;
+
+                                  [Mappa]
+                                  public sealed partial class Mapper
+                                  {
+                                      public partial (string, string) Map((int Alfa, string Beta) input);
+                                  }
+                                  """;
+
+        // Act
+        var generatedResults = await RunMappaGeneratorAsync(sourceCode, CancellationToken.None).ConfigureAwait(true);
+
+       // Assert
+        generatedResults.Should()
+            .NotHaveDiagnostics()
+            .HaveGeneratedSourceCode()
+            .WithCompilationUnit()
+            .NotBeNull().And
+            .HaveDefaultMapMethod(
+                "(string, string)",
+                NullableAnnotation.NotAnnotated,
+                "(int Alfa, string Beta)",
+                NullableAnnotation.NotAnnotated,
+                blockSyntaxAssertions =>
+                {
+                    blockSyntaxAssertions
+                        .HasSyntaxNodesCount(5)
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("int", "__mappa_tmp_1", initializationAssertions => initializationAssertions.BeMemberAccessExpressionSyntax("input.Item1"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("string", "__mappa_tmp_2", initializationAssertions => initializationAssertions.BeInvocationExpressionSyntax("__mappa_tmp_1.ToString"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax("string", "__mappa_tmp_3", initializationAssertions => initializationAssertions.BeMemberAccessExpressionSyntax("input.Item2"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax(
+                                "(string, string)",
                                 "__mappa_tmp_4",
                                 initializationAssertions => initializationAssertions.BeTupleExpressionSyntax(
                                         parametersAssertions => parametersAssertions.BeIdentifierNameSyntax("__mappa_tmp_2"),
