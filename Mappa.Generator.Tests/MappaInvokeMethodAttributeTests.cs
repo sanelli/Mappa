0baadad8c5ac0f7252cea16e21ee8b1bf697686b
Stefano Anelli <stefano.anelli@gmail.com>
2024-10-17 19:27:39 +0100

    #54 Added more tests

--- Mappa.Generator.Tests/MappaInvokeMethodAttributeTests.cs
+++ Mappa.Generator.Tests/MappaInvokeMethodAttributeTests.cs
@@ -203,13 +203,116 @@ public sealed class MappaInvokeMethodAttributeTests
 
     /// <summary>
     /// Test <see cref="MappaInvokeMethodAttribute"/> targeting
+    /// a non-<c>static</c> local method with one parameter
+    /// with type source class type when multiple methods with
+    /// lower priorities are available.
+    /// </summary>
+    /// <returns>The async task.</returns>
+    [Fact]
+    [IntegrationTest]
+    public async Task CanMapUsingCorrectNonStaticLocalMethodWhenMultipleAvilable()
+    {
+        // Arrange
+        const string sourceCode = """
+                                  #nullable enable
+                                  using Mappa.Attributes;
+
+                                  namespace Mappa.Generator.Tests.UnitTests.SourceCode;
+
+                                  public class Source
+                                  {
+                                      public int PropertyA { get; set; }
+                                      public int PropertyB { get; set; }
+                                  }
+
+                                  public class Target
+                                  {
+                                      public string PropertyA { get; set; }
+                                      public long PropertyB { get; set; }
+                                  }
+
+                                  [Mappa]
+                                  public sealed partial class Mapper
+                                  {
+                                      [MappaInvokeMethodAttribute("PropertyA", nameof(CustomMapPropertyA))]
+                                      public partial Target Map(Source input);
+                                      
+                                      public string CustomMapPropertyA(Source source)
+                                      {
+                                         return $"{source.PropertyA} - {source.PropertyB}";
+                                      }
+                                      
+                                      public string CustomMapPropertyA()
+                                      {
+                                         return "constant";
+                                      }
+                                  }
+                                  #nullable restore
+                                  """;
+
+        // Act
+        var generatedResults = await RunMappaGeneratorAsync(sourceCode, CancellationToken.None).ConfigureAwait(true);
+
+        // Assert
+        generatedResults.Should()
+            .NotHaveDiagnostics()
+            .HaveGeneratedSourceCode()
+            .WithCompilationUnit()
+            .NotBeNull().And
+            .HaveDefaultMapMethod(
+                "Mappa.Generator.Tests.UnitTests.SourceCode.Target",
+                NullableAnnotation.NotAnnotated,
+                "Mappa.Generator.Tests.UnitTests.SourceCode.Source",
+                NullableAnnotation.NotAnnotated,
+                blockSyntaxAssertions =>
+                {
+                    blockSyntaxAssertions
+                        .HasSyntaxNodesCount(4)
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax(
+                                typeof(string).ToString(),
+                                "__mappa_tmp_1",
+                                initializationAssertions => initializationAssertions.BeInvocationExpressionSyntax(
+                                    "this.CustomMapPropertyA",
+                                    firstParameterAssertions => firstParameterAssertions.BeIdentifierNameSyntax("input")));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax(
+                                typeof(int).ToString(),
+                                "__mappa_tmp_2",
+                                initializationAssertions => initializationAssertions.BeMemberAccessExpressionSyntax("input.PropertyB"));
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax(
+                                "Mappa.Generator.Tests.UnitTests.SourceCode.Target",
+                                "__mappa_tmp_3",
+                                initializationAssertions =>
+                                {
+                                    initializationAssertions.BeObjectCreationExpressionSyntax(
+                                        "Mappa.Generator.Tests.UnitTests.SourceCode.Target",
+                                        ("PropertyA", initAssertions => initAssertions.BeIdentifierNameSyntax("__mappa_tmp_1")),
+                                        ("PropertyB", initAssertions => initAssertions.BeIdentifierNameSyntax("__mappa_tmp_2")));
+                                });
+                        })
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                        {
+                            syntaxNodeAssertions.BeReturnStatement(assertion => assertion.BeIdentifierNameSyntax("__mappa_tmp_3"));
+                        });
+                });
+    }
+
+    /// <summary>
+    /// Test <see cref="MappaInvokeMethodAttribute"/> targeting
     /// a non-<c>static</c> local <c>static</c> method with one parameter
     /// with type source class type.
     /// </summary>
     /// <returns>The async task.</returns>
     [Fact]
     [IntegrationTest]
-    public async Task CanMapUsingNonStaticLocalStaticMethod()
+    public async Task CanMapUsingStaticLocalMethod()
     {
         // Arrange
         const string sourceCode = """
--- Mappa.Generator/Algorithm/StrategyDetectors/ConstructorMapStrategyDetector.cs
+++ Mappa.Generator/Algorithm/StrategyDetectors/ConstructorMapStrategyDetector.cs
@@ -496,6 +496,7 @@ internal sealed class ConstructorMapStrategyDetector
             // If only one method has been found happy days!
             if (methodsWithTheRightNameAndReturnType.Length == 1)
             {
+                // TODO [#54] Even if only 1 method is available might not be suitable (e.g. method has 2 params - source and property - but source property is not available.
                 return methodsWithTheRightNameAndReturnType.Single();
             }
 
