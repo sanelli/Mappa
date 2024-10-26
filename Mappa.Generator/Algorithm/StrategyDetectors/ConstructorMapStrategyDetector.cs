7c5c1cd4d0855653d4b7b5c12ade86c2b56cd85a
Stefano Anelli <stefano.anelli@gmail.com>
2024-10-26 17:07:50 +0100

    #55 Updated TODOs

--- Mappa.Generator/Algorithm/StrategyDetectors/ConstructorMapStrategyDetector.cs
+++ Mappa.Generator/Algorithm/StrategyDetectors/ConstructorMapStrategyDetector.cs
@@ -380,7 +380,7 @@ internal sealed class ConstructorMapStrategyDetector
         }
     }
 
-    // TODO [#55] Add support for MappaAssignFromContext.
+    // TODO [#70] Add support for MappaAssignFromContext.
     private bool TryGetStrategyForPropertyOrArgumentUsingAttributesOnMethod(
         string targetName,
         ITypeSymbol targetType,
