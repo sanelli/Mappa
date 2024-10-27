6c95df49bf1dd65341914df5278b530aafb20015
Stefano Anelli <stefano.anelli@gmail.com>
2024-10-27 10:23:42 +0000

    #56 Added MappaSettingsAttribute

---
+++ Mappa/Attributes/MappaSettingsAttribute.cs
@@ -0,0 +1,76 @@
+// <copyright file="MappaSettingsAttribute.cs" company="Stefano Anelli">
+// Copyright (c) Stefano Anelli. All rights reserved.
+// </copyright>
+
+using System.Globalization;
+
+namespace Mappa.Attributes;
+
+/// <summary>
+/// Allow to specify advanced settings for fine-tuning the mappings.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+public sealed class MappaSettingsAttribute
+    : Attribute
+{
+    /// <summary>
+    /// The type of <see cref="CultureInfo"/> to apply.
+    /// </summary>
+    public enum CultureInfoSettigs
+    {
+        /// <summary>
+        /// Unspecified.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Use the <see cref="CultureInfo.CurrentCulture"/>.
+        /// </summary>
+        CurrentCulture,
+
+        /// <summary>
+        /// Use the <see cref="CultureInfo.CurrentCulture"/>.
+        /// </summary>
+        InvariantCulture,
+
+        /// <summary>
+        /// Allow to user to specify a culture setting via <see cref="CultureName"/>.
+        /// </summary>
+        UserDefined,
+    }
+
+    /// <summary>
+    /// Gets or sets the format when using <see cref="DateTime.ToString(string,System.IFormatProvider)"/> or <see cref="DateTime.ParseExact(string,string,System.IFormatProvider)"/>.
+    /// </summary>
+    public string? DateTimeFormat { get; set; }
+
+    /// <summary>
+    /// Gets or sets the format when using <c>DateOnly.ToString(string,System.IFormatProvider)</c> or <c>DateOnly.ParseExact(string,string,System.IFormatProvider)</c>.
+    /// </summary>
+    public string? DateOnlyFormat { get; set; }
+
+    /// <summary>
+    /// Gets or sets the format when using <c>TimeOnly.ToString(string,System.IFormatProvider)</c> or <c>TimeOnly.ParseExact(string,string,System.IFormatProvider)</c>.
+    /// </summary>
+    public string? TimeOnlyFormat { get; set; }
+
+    /// <summary>
+    /// Gets or sets the format when using <see cref="TimeSpan.ToString(string,System.IFormatProvider)"/> or <see cref="TimeSpan.ParseExact(string,string,System.IFormatProvider)"/>.
+    /// </summary>
+    public string? TimeSpanFormat { get; set; }
+
+    /// <summary>
+    /// Gets or sets the format when using <see cref="Guid.ToString(string)"/> or <see cref="Guid.ParseExact(string,string)"/>.
+    /// </summary>
+    public string? GuidFormat { get; set; }
+
+    /// <summary>
+    /// Gets or sets the <see cref="CultureInfo"/> to use when converting to string or parsing form string.
+    /// </summary>
+    public CultureInfoSettigs CultureInfoSetting { get; set; } = CultureInfoSettigs.CurrentCulture;
+
+    /// <summary>
+    /// Gets or sets the culture name when <see cref="CultureInfoSetting"/> is <see cref="CultureInfoSettigs.UserDefined"/>.
+    /// </summary>
+    public string? CultureName { get; set; }
+}
