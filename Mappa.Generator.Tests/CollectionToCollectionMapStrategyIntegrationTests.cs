2d3fa99f0f9f7b57bcf16c9e60bb8ac7e8c7c9d5
Stefano Anelli <stefano.anelli@gmail.com>
2025-03-22 10:44:58 +0000

    #105 Fixed TODOs

--- Mappa.Generator.Tests/CollectionToCollectionMapStrategyIntegrationTests.cs
+++ Mappa.Generator.Tests/CollectionToCollectionMapStrategyIntegrationTests.cs
@@ -12,31 +12,30 @@ namespace Mappa.Generator.Tests;
 /// <summary>
 /// Integration tests for the <see cref="CollectionToCollectionMapStrategy"/>.
 /// </summary>
-// TODO [#105] int[] -> string[].
-// TODO [#105] int[] -> Span<string>.
-// TODO [#105] int[] -> ReadOnlySpan<string>.
-// TODO [#105] int[] -> Memory<string>.
-// TODO [#105] int[] -> ReadOnlyMemory<string>.
 // TODO [#105] ICollection<int> -> string[].
 // TODO [#105] non-generic impl ICollection<int> -> string[].
 // TODO [#105] generic impl ICollection<int> -> string[].
-// TODO [#105] ICollection<int> -> Span<string>.
-// TODO [#105] ICollection<int> -> ReadOnlySpan<string>.
-// TODO [#105] ICollection<int> -> Memory<string>.
-// TODO [#105] ICollection<int> -> ReadOnlyMemory<string>.
 // TODO [#105] IEnumerable<int> -> string[].
 // TODO [#105] non-generic impl IEnumerable<int> -> string[].
 // TODO [#105] generic impl IEnumerable<int> -> string[].
-// TODO [#105] IEnumerable<int> -> Span<long>.
-// TODO [#105] IEnumerable<int> -> ReadOnlySpan<long>.
-// TODO [#105] IEnumerable<int> -> Memory<long>.
-// TODO [#105] IEnumerable<int> -> ReadOnlyMemory<long>.
 // TODO [#105] IList<int> -> string[].
 // TODO [#105] non-generic impl IList<int> -> string[].
 // TODO [#105] generic impl IList<int> -> string[].
 // TODO [#105] IReadOnlyCollection<int> -> string[].
 // TODO [#105] non-generic impl IReadOnlyCollection<int> -> string[].
 // TODO [#105] generic impl IReadOnlyCollection<int> -> string[].
+// TODO [#105] int[] -> Span<string>.
+// TODO [#105] int[] -> ReadOnlySpan<string>.
+// TODO [#105] int[] -> Memory<string>.
+// TODO [#105] int[] -> ReadOnlyMemory<string>.
+// TODO [#105] ICollection<int> -> Span<string>.
+// TODO [#105] ICollection<int> -> ReadOnlySpan<string>.
+// TODO [#105] ICollection<int> -> Memory<string>.
+// TODO [#105] ICollection<int> -> ReadOnlyMemory<string>.
+// TODO [#105] IEnumerable<int> -> Span<long>.
+// TODO [#105] IEnumerable<int> -> ReadOnlySpan<long>.
+// TODO [#105] IEnumerable<int> -> Memory<long>.
+// TODO [#105] IEnumerable<int> -> ReadOnlyMemory<long>.
 // TODO [#105] ERROR WHEN FROM IEnumerable<int> -> Custom<string> without empty param constructor.
 public class CollectionToCollectionMapStrategyIntegrationTests
     : MappaGeneratorAbstractUnitTests
