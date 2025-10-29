8c0f54557752c2355ac12c032f213ca59a45f6ef
Stefano Anelli <stefano.anelli@gmail.com>
2025-10-29 23:32:51 +0000

    #109 Added more tests

--- Mappa.Generator.Tests/ContainerCapacityConstructorsIntegrationTests.cs
+++ Mappa.Generator.Tests/ContainerCapacityConstructorsIntegrationTests.cs
@@ -204,9 +204,108 @@ public sealed class ContainerCapacityConstructorsIntegrationTests
                 });
     }
 
-    // TODO [#109] Test Mappa setting on class enabled with custom collection with constructor with 1 parameter & 0 parameters.
+    /// <summary>
+    /// Test map from array to custom collection type implementing the
+    /// <see cref="ICollection{T}"/> interface having one constructor
+    /// with one integer parameter (capacity).
+    /// The <see cref="MappaSettingsAttribute.ContainerCapacityConstructors"/>
+    /// is enabled on the class therefore the custom container is
+    /// invoked with the constructor with one parameter.
+    /// In this test a constructor without parameters also exists but it
+    /// is not going to be picked up.
+    /// </summary>
+    /// <returns>The async task.</returns>
+    [Fact]
+    [IntegrationTest]
+    public async Task CanMapUsingCustomCollectionWithCapacityConstructorAndEmptyConstructorWhenConstructorWithOneIntegerParameterExistsAndSettingEnabledOnClass()
+    {
+        // Arrange
+        const string sourceCode = """
+                                  #nullable enable
+
+                                  using Mappa.Attributes;
+                                  using System.Collections.Generic;
+
+                                  namespace Mappa.Generator.Tests.UnitTests.SourceCode;
+                                  
+                                  public partial class Target : ICollection<string>
+                                  {
+                                    public Target() { }
+                                    public Target(int capacity) { }
+                                  }
+
+                                  [Mappa]
+                                  [MappaSettings(ContainerCapacityConstructors = BooleanSetting.Enable)]
+                                  public sealed partial class Mapper
+                                  {
+                                      public partial Target Map(int[] input);
+                                  }
+
+                                  #nullable restore
+                                  """;
+
+        // Act
+        var generatedResults = await RunMappaGeneratorAsync(sourceCode, CancellationToken.None).ConfigureAwait(true);
+
+        // Assert
+        generatedResults.Should()
+            .NotHaveDiagnostics()
+            .HaveGeneratedSourceCode()
+            .WithCompilationUnit()
+            .NotBeNull().And
+            .HaveDefaultMapMethod(
+                "Mappa.Generator.Tests.UnitTests.SourceCode.Target",
+                NullableAnnotation.NotAnnotated,
+                typeof(int[]).ToString(),
+                NullableAnnotation.NotAnnotated,
+                blockSyntaxAssertions =>
+                {
+                    blockSyntaxAssertions
+                        .HasSyntaxNodesCount(3)
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                            syntaxNodeAssertions.BeLocalDeclarationStatementSyntax(
+                                "Mappa.Generator.Tests.UnitTests.SourceCode.Target",
+                                "__mappa_tmp_1",
+                                initializerAssertions => initializerAssertions.BeObjectCreationExpressionSyntax(
+                                    "Mappa.Generator.Tests.UnitTests.SourceCode.Target",
+                                    parameterAssertions => parameterAssertions.BeMemberAccessExpressionSyntax("input.Length"))))
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                            syntaxNodeAssertions.BeForStatementSyntax(
+                                declarationAssertions => declarationAssertions.BeAssignmentFromConstant(typeof(int).ToString(), "__mappa_tmp_2", 0),
+                                conditionAssertions => conditionAssertions.BeBinaryExpressionSyntax(
+                                    leftExpressionAssertions => leftExpressionAssertions.BeIdentifierNameSyntax("__mappa_tmp_2"),
+                                    SyntaxKind.LessThanToken,
+                                    rightExpressionAssertions => rightExpressionAssertions.BeMemberAccessExpressionSyntax("input.Length")),
+                                incrementorAssertions => incrementorAssertions.BePrefixUnaryExpressionSyntax(SyntaxKind.PlusPlusToken, operandAssertions => operandAssertions.BeIdentifierNameSyntax("__mappa_tmp_2")),
+                                statementAssertions =>
+                                    statementAssertions
+                                        .BeBlockStatement()
+                                        .AsBlock()
+                                        .HasSyntaxNodesCount(3)
+                                        .HasNextSyntaxNode(foreachStatementAssertions =>
+                                            foreachStatementAssertions.BeLocalDeclarationStatementSyntax(
+                                                typeof(int).ToString(),
+                                                "__mappa_tmp_3",
+                                                initializerAssertions => initializerAssertions.BeElementAccessExpressionSyntaxWithIdentifierNameSyntax("input", "__mappa_tmp_2")))
+                                        .HasNextSyntaxNode(foreachStatementAssertions =>
+                                            foreachStatementAssertions.BeLocalDeclarationStatementSyntax(
+                                                typeof(string).ToString(),
+                                                "__mappa_tmp_4",
+                                                initializerAssertions => initializerAssertions.BeInvocationExpressionSyntax("__mappa_tmp_3.ToString")))
+                                        .HasNextSyntaxNode(foreachStatementAssertions =>
+                                            foreachStatementAssertions.BeInvocationExpressionSyntaxStatement(
+                                                "__mappa_tmp_1.Add",
+                                                firstParameterAssertions => firstParameterAssertions.BeIdentifierNameSyntax("__mappa_tmp_4")))))
+                        .HasNextSyntaxNode(syntaxNodeAssertions =>
+                            syntaxNodeAssertions.BeReturnStatement(expressionAssertions => expressionAssertions
+                                .BeIdentifierNameSyntax("__mappa_tmp_1")));
+                });
+    }
+
     // TODO [#109] Test Mappa setting on class disabled with custom collection with constructor with 1 parameter & 0 parameters.
     // TODO [#109] Test Mappa setting on class unset with custom collection with constructor with 1 parameter & 0 parameters.
+    // TODO [#109] Test where source is IEnumerable to make sure the empty constructor is picked up when also present.
+    // TODO [#109] Test where source is IEnumerable to make sure the capacity is computed using LINQ Count() method.
     // TODO [#109] Test Mappa setting on on method disabled.
     // TODO [#109] Test Mappa setting on on method undefined.
 }
