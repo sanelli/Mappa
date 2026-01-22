edc988270d1d3ad4bee0ad7094db318f934106cb
Stefano Anelli <stefano.anelli@gmail.com>
2026-01-22 21:43:52 +0000

    #49 Add Samples

--- Mappa.Samples.Tests/PolymorphicMethodMapMapperUnitTests.cs
+++ Mappa.Samples.Tests/PolymorphicMethodMapMapperUnitTests.cs
@@ -16,6 +16,7 @@ public sealed class PolymorphicMethodMapMapperUnitTests
 {
     private static readonly PolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingAttributeMapper PolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingAttributeMapper = new();
     private static readonly PolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingDefaultAttributeMapper PolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingDefaultAttributeMapper = new();
+    private static readonly PolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingAttributeUsingFieldDependencyMapper PolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingAttributeUsingFieldDependencyMapper = new();
 
     /// <summary>
     /// Test <see cref="PolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingAttributeMapper.Map(Models.Polymorphism.One.SourceWithDependency)"/>.
@@ -70,5 +71,33 @@ public sealed class PolymorphicMethodMapMapperUnitTests
         target.NestedProperty.Should().NotBeNull();
         target.NestedProperty.Should().BeOfType<Models.Polymorphism.One.TargetUnmappedBaseClass>();
         target.NestedProperty.NumericProperty.Should().Be(456);
+    }
+
+    /// <summary>
+    /// Test <see cref="Samples.PolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingAttributeUsingFieldDependencyMapper.Map(Models.Polymorphism.One.SourceWithDependency)"/>.
+    /// </summary>
+    [Fact]
+    [UnitTest]
+    private void TestPolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingAttributeMapperUsingFieldDependency()
+    {
+        // Arrange
+        var source = new Models.Polymorphism.One.SourceWithDependency
+        {
+            NumericProperty = 125,
+            NestedProperty = new Models.Polymorphism.One.SourceThirdClass
+            {
+                NumericProperty = 456, GuidProperty = Guid.NewGuid(), Numbers = ["7", "8", "9"],
+            },
+        };
+
+        // Act
+        var target = PolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingAttributeUsingFieldDependencyMapper.Map(source);
+
+        // Assert
+        target.NumericProperty.Should().Be(125L);
+        target.NestedProperty.Should().NotBeNull();
+        target.NestedProperty.NumericProperty.Should().Be(456);
+        target.NestedProperty.GuidProperty.Should().Be(source.NestedProperty.GuidProperty.ToString());
+        target.NestedProperty.Numbers.Should().BeEquivalentTo([7, 8, 9]);
     }
 }
--- Mappa.Samples/PolymorphicMethodMapMapper.cs
+++ Mappa.Samples/PolymorphicMethodMapMapper.cs
@@ -53,9 +53,10 @@ public sealed partial class PolymorphicMethodMapMapperIdentifiedViaMappaTypeMapp
 /// polymorphic methods when source and target are defined via
 /// <see cref="MappaTypeMappingDefaultAttribute"/>.
 /// </summary>
-// TODO [#49] This should only work when a specific [MappaSetting(PolymorphicMapMethodWithMatchingDefaultAttribute)] is enabled to support mapping using MappaTypeMappingDefault.
 [Mappa]
-[MappaSettings(CultureInfoSetting = CultureInfoSetting.InvariantCulture)]
+[MappaSettings(
+    CultureInfoSetting = CultureInfoSetting.InvariantCulture,
+    PolymorphicMapMethodWithMatchingDefaultAttribute = BooleanSetting.Enable)]
 public sealed partial class PolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingDefaultAttributeMapper
 {
     /// <summary>
@@ -85,4 +86,54 @@ public sealed partial class PolymorphicMethodMapMapperIdentifiedViaMappaTypeMapp
     /// <param name="source">The source model.</param>
     /// <returns>The target model.</returns>
     public partial Models.Polymorphism.One.TargetWithDependencyWithUnmappedBaseClass Map(Models.Polymorphism.One.SourceWithDependencyWithSourceBaseClass source);
+}
+
+/// <summary>
+/// Mapper thet contain a method that can be used for dependencies.
+/// </summary>
+[Mappa]
+[MappaSettings(
+    CultureInfoSetting = CultureInfoSetting.InvariantCulture)]
+public sealed partial class PolymorphicMethodMapDependency
+{
+    /// <summary>
+    /// Map from <see cref="Models.Polymorphism.One.SourceBaseClass"/>
+    /// to <see cref="Models.Polymorphism.One.TargetBaseClass"/> by supporting
+    /// polymorphism.
+    /// This will use the <see cref="MappaTypeMappingDefaultAttribute"/> default
+    /// value (i.e. throw if the input is neither of the expected input values).
+    /// </summary>
+    /// <param name="source">The source model.</param>
+    /// <returns>The target model.</returns>
+    [MappaTypeMapping(typeof(Models.Polymorphism.One.TargetFirstClass), typeof(Models.Polymorphism.One.SourceFirstClass))]
+    [MappaTypeMapping(typeof(Models.Polymorphism.One.TargetThirdClass), typeof(Models.Polymorphism.One.SourceThirdClass))]
+    [MappaTypeMapping(typeof(Models.Polymorphism.One.TargetSecondClass), typeof(Models.Polymorphism.One.SourceSecondClass))]
+    public partial Models.Polymorphism.One.TargetBaseClass Map(Models.Polymorphism.One.SourceBaseClass source);
+}
+
+/// <summary>
+/// Mapper to showcase the ability of Mappa to pick up
+/// polymorphic methods when source and target are defined via
+/// <see cref="MappaTypeMappingAttribute"/>.
+/// </summary>
+[Mappa]
+[MappaSettings(CultureInfoSetting = CultureInfoSetting.InvariantCulture)]
+public sealed partial class PolymorphicMethodMapMapperIdentifiedViaMappaTypeMappingAttributeUsingFieldDependencyMapper
+{
+    [MappaDependency]
+    private readonly PolymorphicMethodMapDependency dependency = new();
+
+    /// <summary>
+    /// Maps from <see cref="Models.Polymorphism.One.SourceWithDependency"/> to
+    /// <see cref="Models.Polymorphism.One.TargetWithDependency"/>
+    /// using <see cref="PolymorphicMethodMapDependency.Map(Models.Polymorphism.One.SourceBaseClass)"/> because it
+    /// support the nested property type mapping using the mapping
+    /// defined via <see cref="MappaTypeMappingAttribute"/>
+    /// from <see cref="Models.Polymorphism.One.SourceThirdClass"/>
+    /// to <see cref="Models.Polymorphism.One.TargetThirdClass"/>
+    /// on a dependency field.
+    /// </summary>
+    /// <param name="source">The source model.</param>
+    /// <returns>The target model.</returns>
+    public partial Models.Polymorphism.One.TargetWithDependency Map(Models.Polymorphism.One.SourceWithDependency source);
 }
