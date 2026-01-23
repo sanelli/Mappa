c1f81efdee990f0d79d7728d375e30966f0e4abb
Stefano Anelli <stefano.anelli@gmail.com>
2026-01-23 18:25:04 +0000

    #49 Updated error codes description.

--- Mappa.Generator/Models/Strategies/PolymorphismMapStrategy.cs
+++ Mappa.Generator/Models/Strategies/PolymorphismMapStrategy.cs
@@ -19,7 +19,6 @@ namespace Mappa.Generator.Models.Strategies;
 /// <param name="defaultBehaviorStrategy">Strategy to map <see cref="MappaTypeMappingDefaultBehavior.MapSourceType"/> behavior.</param>
 /// <param name="nullableEnabled"><c>true</c> if nullable is enabled, <c>false</c> otherwise.</param>
 /// <param name="mapMethodContextParameterName">The name of the map method context parameter (if present).</param>
-// TODO [#49] Update Documentation/error-codes.md.
 internal sealed class PolymorphismMapStrategy(
     ITypeSymbol targetType,
     ITypeSymbol sourceType,
